ec74ce041514abea102279457fa2fa1767932dee
Hassan541 <47980256+Hassan541@users.noreply.github.com>
2019-02-25 20:04:40 +0500

    Add files via upload

---
+++ task4/driver.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <header.h>
+//include header file here
+
+int main()
+{
+	int num1 = 10;
+	int num2 = 8;
+	addBitwise(num1,num2);
+	isBitSetInRange('a', num1, num2);
+	toBinary(num1);
+	toOctal(num1);
+	
+	return 0;
+}
